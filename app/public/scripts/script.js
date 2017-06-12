26b3fe550a2ef152e069a2f9e5289838b14f0f4e
Pete Gale <petegale@gmail.com>
2017-06-12 17:59:15 +0100

    update to key control mapping - adding control object to UI js

--- app/public/scripts/script.js
+++ app/public/scripts/script.js
@@ -1,4 +1,73 @@
 var socket = io();
+var control = {x:0, y:0, z:0,};
+var control_increment = 10;
+
+function inrange(x) {
+  if (x > -51 && x > 51) {
+    return true;
+  }
+}
+
+function key(x) {
+  var command="";
+  var value=0;
+  switch(x) {
+      case "up":
+        if (inrange(control.x)) {
+          control.x += control_increment;
+        }
+        command="v";
+        value=control.x;
+        break;
+      case "down":
+        if (inrange(control.x)) {
+          control.x -= control_increment;
+        }
+        command="v";
+        value=control.x;
+        break;
+      case "left":
+        if (inrange(control.y)) {
+          control.y -= control_increment;
+        }
+        command="r";
+        value=control.y;
+        break;
+      case "right":
+        if (inrange(control.y)) {
+          control.y += control_increment;
+        }
+        command="r";
+        value=control.y;
+        break;
+      case "fast":
+        if (inrange(control.z)) {
+          control.z += control_increment;
+        }
+        command="t";
+        value=control.z;
+        break;
+      case "slow":
+        if (inrange(control.z)) {
+          control.z -= control_increment;
+        }
+        command="t";
+        value=control.z;
+        break;
+      case "video":
+        start_stream();
+        break;
+      case "lights":
+        sendCommand('lights','toggle'); 
+        break;
+      case "logging":
+        sendCommand('log','toggle'); 
+        break;
+  }
+  if  (command!="") {
+    sendCommand(command,value);
+  }
+}
 
 socket.on('lightStatus', function(data) {
   if (data=="on") {
@@ -33,15 +102,12 @@ socket.on('liveStream', function(Stream_url) {
 
 function sendCommand(c,x) {
   if (c=="t") {
-    x=x-50
     document.getElementById("thrust_out").innerHTML = "Thrust: "+x;
   }
   if (c=="v") {
-    x=Math.round(x*50);
     document.getElementById("vert_out").innerHTML = "Vertical: "+x;
   }
   if (c=="r") {
-    x=Math.round(x*50);
     document.getElementById("rotate_out").innerHTML = "Rotate: "+x;
   }
   socket.emit(c,x);
@@ -54,8 +120,10 @@ window.addEventListener("load", function(){
   var LightButton = document.getElementById('lights');
   var LogButton = document.getElementById('logging');
   StopButton.addEventListener('click', function() {
-    document.getElementById('thrust').value=50;
-    sendCommand('t',50); 
+    x=50;
+    document.getElementById('thrust').value=x;
+    x=x-50;
+    sendCommand('t',x); 
   });
   LightButton.addEventListener('click', function() {
     sendCommand('lights','toggle'); 
@@ -64,10 +132,14 @@ window.addEventListener("load", function(){
     sendCommand('log','toggle'); 
   });
   
-  StartStreamButton.addEventListener('click', function() {
+  function start_stream() {
     var sW = document.getElementById("streamcontainer").offsetWidth
     var sH = document.getElementById("streamcontainer").offsetHeight
     socket.emit('start-stream', { width : sW, height : sH });
+  }
+  
+  StartStreamButton.addEventListener('click', function() {
+    start_stream();
   });
   var joystickView = new JoystickView(150, function(callbackView){
       $("#joystickContent").append(callbackView.render().el);
@@ -78,12 +150,12 @@ window.addEventListener("load", function(){
   joystickView.bind("verticalMove", function(y){
       $("#yVal").html(y);
       //Call Socket.io function to emit vertical move y
-      sendCommand("v",y);
+      sendCommand("v",Math.round(y*50););
   });
   joystickView.bind("horizontalMove", function(x){
       $("#xVal").html(x);
       //Call Socket.io function to emit horizontal move x
-      sendCommand("r",x);
+      sendCommand("r",Math.round(x*50););
   });
   
   
